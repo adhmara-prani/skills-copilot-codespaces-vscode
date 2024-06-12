60b020d4f565beb3a62882d81c06c9ca4d6f3b78
adhmara-prani <159698679+adhmara-prani@users.noreply.github.com>
2024-06-12 12:22:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,2 @@
-// Create a web server
-// Load the 'http' module
-var http = require('http');
-
-// Create a web server
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<html><body>' +
-        '<h1>Comments</h1>' +
-        '<form method="post" action="/comments">' +
-        '<textarea name="comment" rows="4" cols="50"></textarea><br>' +
-        '<input type="submit" value="Submit">' +
-        '</form>' +
-        '</body></html>');
-    response.end();
-}).listen(3000);
-
-console.log('Server running at http://localhost:3000/comments');
+// Create web server and listen to port 3000
+// Return the comments in JSON format
